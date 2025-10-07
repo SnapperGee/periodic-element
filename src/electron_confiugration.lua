d7c4ff977ecc54bd652de00c93a1a7f81ef7b50a
Snap <XxSnapperGeexX@gmail.com>
2025-10-06 17:12:02 -0700

    Start adding properties to ElectronConfiguration prototype.

--- src/electron_confiugration.lua
+++ src/electron_confiugration.lua
@@ -1,5 +1,7 @@
 local SubshellOccupancy = require("src.subshell_occupancy")
 
+local NOBLE_GAS_SYMBOLS = { He = true, Ne = true, Ar = true, Kr = true, Xe = true, Rn = true }
+
 ---@class ElectronConfiguration
 ---@field core string|nil    -- noble-gas symbol like "He","Ne","Ar","Kr","Xe","Rn"
 ---@field subshell_occupancy SubshellOccupancy[] -- ordered list as written (keep authoring order)
@@ -17,7 +19,28 @@ function ElectronConfiguration:new(opts)
 
     assert(type(opts) == "table", "opts table required")
 
-    local obj = setmetatable({}, self)
+    assert(
+        opts.core == nil or type(opts.core) == "string" and #opts.core == 2,
+        string.format("expected 'core' string of nil or 2 characters but got: %s", tostring(opts.core))
+    )
+
+    local normalized_core_string = opts.core and (opts.core:sub(1,1):upper() .. opts.core:sub(2)) or nil
+
+    assert(
+        normalized_core_string == nil or NOBLE_GAS_SYMBOLS[normalized_core_string],
+        string.format("expected 'core' string of nil, 'He','Ne','Ar','Kr','Xe',or 'Rn' but got: %s", opts.core)
+    )
+
+    -- TODO: check for non empty array of SubshellOccupancy objects
+    -- assert(
+    --     type(opts.subshell_occupancy) == "table" and getmetatable(opts.subshell_occupancy) == SubshellOccupancy,
+    --     string.format("expected non empty 'subshell_occupancy' array table with SubshellOccupancy metatable but got: %s", tostring(opts.subshell_occupancy))
+    -- )
+
+    local obj = setmetatable({
+        core = normalized_core_string,
+        subshell_occupancy = opts.subshell_occupancy
+    }, self)
 
     return obj
 end
