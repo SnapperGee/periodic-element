c4050c9d2efc7535d54b7247020548291bff2e6c
Snap <XxSnapperGeexX@gmail.com>
2025-10-08 00:00:14 -0700

    Rename subshell_letter and quantum_number fields of SubshellOccupancy metatable to n and ln.

--- src/subshell_occupancy.lua
+++ src/subshell_occupancy.lua
@@ -1,14 +1,14 @@
 local ELECTRON_CAP = { s = 2, p = 6, d = 10, f = 14 }
 local N_MIN = { s = 1, p = 2, d = 3, f = 4 }
-local SUBSHELL_LETTER_RANK = { s=0, p=1, d=2, f=3 }
+local L_LETTER_RANK = { s=0, p=1, d=2, f=3 }
 
 ---@alias SubshellLetter '"s"'|'"p"'|'"d"'|'"f"'
 
-local VALID_SUBSHELL_LETTER = { s = true, p = true, d = true, f = true }
+local VALID_L_LETTER = { s = true, p = true, d = true, f = true }
 
 ---@class SubshellOccupancy
----@field quantum_number integer         -- principal quantum number (e.g., 3 in 3d10)
----@field subshell_letter SubshellLetter -- subshell letter: s/p/d/f
+---@field n integer        -- principal quantum number (e.g., 3 in 3d10)
+---@field l SubshellLetter -- azimuthal (angular-momentum) quantum number (orbital/subshell type): s/p/d/f
 ---@field electron_count integer         -- electrons occupying that subshell (capacity: s≤2,p≤6,d≤10,f≤14)
 local SubshellOccupancy = {}
 
@@ -20,19 +20,19 @@ end
 
 function SubshellOccupancy.__eq(a, b)
     return rawequal(a,b)
-        or a.quantum_number == b.quantum_number
-        and a.subshell_letter == b.subshell_letter
+        or a.n == b.n
+        and a.l == b.l
         and a.electron_count == b.electron_count
 end
 
 function SubshellOccupancy.__lt(a, b)
     if rawequal(a, b) then return false end
 
-    if a.quantum_number ~= b.quantum_number then
-        return a.quantum_number < b.quantum_number
+    if a.n ~= b.n then
+        return a.n < b.n
     end
 
-    local ra, rb = SUBSHELL_LETTER_RANK[a.subshell_letter], SUBSHELL_LETTER_RANK[b.subshell_letter]
+    local ra, rb = l_RANK[a.l], L_LETTER_RANK[b.subshell_letter]
 
     if ra ~= rb then
         return ra < rb
@@ -46,8 +46,8 @@ function SubshellOccupancy.__le(a, b)
 end
 
 ---@class SubshellOccupancyInitOpts
----@field quantum_number integer         -- principal quantum number (e.g., 3 in 3d10)
----@field subshell_letter SubshellLetter -- subshell letter: s/p/d/f
+---@field n integer         -- principal quantum number (e.g., 3 in 3d10)
+---@field l SubshellLetter -- subshell letter: s/p/d/f
 ---@field electron_count integer         -- electrons occupying that subshell (capacity: s≤2,p≤6,d≤10,f≤14)
 
 -- TODO: Make immutable
@@ -58,30 +58,30 @@ function SubshellOccupancy:new(opts)
     assert(type(opts) == "table", "opts table required")
 
     assert(
-        type(opts.subshell_letter) == "string" and #opts.subshell_letter == 1,
-        string.format("'subshell_letter' must be one character but got: %s", tostring(opts.subshell_letter))
+        type(opts.l) == "string" and #opts.l == 1,
+        string.format("'l' must be one character but got: %s", tostring(opts.l))
     )
 
-    local normalized_subshell_letter = opts.subshell_letter:lower()
+    local normalized_l = opts.l:lower()
 
     assert(
-        VALID_SUBSHELL_LETTER[normalized_subshell_letter],
-        string.format("'subshell_letter' must be one of 's','p','d','f' but got: %s", tostring(opts.subshell_letter))
-    )
-
-    assert(
-        type(opts.quantum_number) == "number" and opts.quantum_number % 1 == 0 and opts.quantum_number >= N_MIN[opts.subshell_letter],
-        string.format("'quantum_number' with subshell '%s' must be integer greater than or equal to %d but got : %s", opts.subshell_letter, N_MIN[opts.subshell_letter], tostring(opts.quantum_number))
+        VALID_L_LETTER[normalized_l],
+        string.format("'l' azimuthal (angular-momentum) quantum number (orbital/subshell type) must be one of 's','p','d','f' but got: %s", tostring(opts.l))
     )
 
     assert(
-        type(opts.electron_count) == "number" and opts.electron_count % 1 == 0 and opts.electron_count >= 1 and opts.electron_count <= ELECTRON_CAP[opts.subshell_letter],
-        string.format("'electron_count' with subshell '%s' must be integer in [1, %d] but got: %s", opts.subshell_letter, ELECTRON_CAP[opts.subshell_letter], tostring(opts.electron_count))
+        type(opts.n) == "number" and opts.n % 1 == 0 and opts.n >= N_MIN[opts.l],
+        string.format("'n' (principal quantum number) with subshell '%s' must be integer greater than or equal to %d but got : %s", opts.l, N_MIN[opts.l], tostring(opts.n))
+    )
+
+    assert(
+        type(opts.electron_count) == "number" and opts.electron_count % 1 == 0 and opts.electron_count >= 1 and opts.electron_count <= ELECTRON_CAP[opts.l],
+        string.format("'electron_count' with subshell '%s' must be integer in [1, %d] but got: %s", opts.l, ELECTRON_CAP[opts.l], tostring(opts.electron_count))
     )
 
     local obj = setmetatable({
-        quantum_number = opts.quantum_number,
-        subshell_letter = normalized_subshell_letter,
+        quantum_number = opts.n,
+        subshell_letter = normalized_l,
         electron_count = opts.electron_count
     }, self)
 
