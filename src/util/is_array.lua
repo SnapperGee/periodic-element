5d611acb5b9e9eeffe55b4599014e8209533671d
Snap <XxSnapperGeexX@gmail.com>
2025-10-14 11:13:54 -0700

    Add annotations to is_array method.

--- src/util/is_array.lua
+++ src/util/is_array.lua
@@ -1,11 +1,14 @@
-local function is_array(obj, check_type)
-    if type(obj) ~= "table" or rawget(obj, 1) == nil then return false end
+---@param arg any
+---@param check_type? string|table|fun(v:any):boolean
+---@return boolean
+local function is_array(arg, check_type)
+    if type(arg) ~= "table" or rawget(arg, 1) == nil then return false end
 
-    for k, v in pairs(obj) do
+    for k, v in pairs(arg) do
         if type(k) ~= "number"
             or k < 1
             or k ~= math.floor(k)
-            or (k ~= 1 and rawget(obj, k-1) == nil)
+            or (k ~= 1 and rawget(arg, k-1) == nil)
             or (type(check_type) == "string" and type(v) ~= check_type)
             or (type(check_type) == "table" and (type(v) ~= "table" or getmetatable(v) ~= check_type))
             or (type(check_type) == "function" and not check_type(v)) then
