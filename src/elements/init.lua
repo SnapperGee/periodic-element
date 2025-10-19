f2c20d8290f63d822d17770f784b11667f8b59b6
Snap <XxSnapperGeexX@gmail.com>
2025-10-18 19:57:43 -0700

    Make elements class immutable.

--- src/elements/init.lua
+++ src/elements/init.lua
@@ -3,43 +3,47 @@ local is_array = require("util.is_array")
 
 local Elements = {}
 
-Elements.__index = Elements
+local DATA = setmetatable({}, { __mode = "k" })
 
-function Elements.__newindex()
-    error("Elements records are immutable", 2)
-end
+local METATABLE = {
+    __index = function(self, k)
+        return Elements[k]
+    end,
+    __newindex = function(self, k, v)
+        error("Elements records are immutable", 2)
+    end,
+    __call = function(self, atomic_number_or_symbol_or_name)
+        local self_data = DATA[self]
 
----@param atomic_number_or_symbol_or_name number|string
----@return Element|nil
-function Elements:__call(atomic_number_or_symbol_or_name)
-    if type(atomic_number_or_symbol_or_name) == "number" then
-        assert(
-            atomic_number_or_symbol_or_name == math.floor(atomic_number_or_symbol_or_name) and atomic_number_or_symbol_or_name >= 1 and atomic_number_or_symbol_or_name <= 118,
-            "Atomic number integer in [1, 118] expected but got: " .. tostring(atomic_number_or_symbol_or_name)
-        )
+        if type(atomic_number_or_symbol_or_name) == "number" then
+            assert(
+                atomic_number_or_symbol_or_name == math.floor(atomic_number_or_symbol_or_name) and atomic_number_or_symbol_or_name >= 1 and atomic_number_or_symbol_or_name <= 118,
+                "Atomic number integer in [1, 118] expected but got: " .. tostring(atomic_number_or_symbol_or_name)
+            )
 
-        return self.atomic_number_index[atomic_number_or_symbol_or_name]
-    end
-
-    if type(atomic_number_or_symbol_or_name) == "string" then
-        assert(
-            #atomic_number_or_symbol_or_name ~= 0,
-            "non empty symbol or name string required"
-        )
-
-        local normalized_symbol_or_name = atomic_number_or_symbol_or_name:sub(1,1):upper() .. atomic_number_or_symbol_or_name:sub(2):lower()
-
-        if #normalized_symbol_or_name <= 2 then
-            return self.symbol_index[normalized_symbol_or_name]
-        else
-            return self.name_index[normalized_symbol_or_name]
+            return self_data.atomic_number_index[atomic_number_or_symbol_or_name]
         end
-    end
 
-    error("element string symbol or name or integer atomic number expected but got: " .. tostring(type(atomic_number_or_symbol_or_name)), 2)
-end
+        if type(atomic_number_or_symbol_or_name) == "string" then
+            assert(
+                #atomic_number_or_symbol_or_name ~= 0,
+                "non empty symbol or name string required"
+            )
 
--- TODO: Make immutable
+            local normalized_symbol_or_name = atomic_number_or_symbol_or_name:sub(1,1):upper() .. atomic_number_or_symbol_or_name:sub(2):lower()
+
+            if #normalized_symbol_or_name <= 2 then
+                return self_data.symbol_index[normalized_symbol_or_name]
+            else
+                return self_data.name_index[normalized_symbol_or_name]
+            end
+        end
+
+        error("element string symbol or name or integer atomic number expected but got: " .. tostring(type(atomic_number_or_symbol_or_name)), 2)
+    end,
+    __metatable = Elements
+}
+
 function Elements:new(elements)
 
     assert(type(elements) == "table", "elements table required")
@@ -65,11 +69,15 @@ function Elements:new(elements)
         name_index[element.name] = element
     end
 
-    return setmetatable({
+    local obj = setmetatable({}, METATABLE)
+
+    DATA[obj] = {
         atomic_number_index = atomic_number_index,
         symbol_index = symbol_index,
         name_index = name_index
-    }, self)
+    }
+
+    return obj
 end
 
 return Elements
