bed3d36eb3a9b22959e8d2166229f96685a0e98b
Snap <XxSnapperGeexX@gmail.com>
2025-11-06 00:42:30 -0800

    Remove debug print from test statement.

--- test/periodic-element/element_spec.lua
+++ test/periodic-element/element_spec.lua
@@ -2,7 +2,6 @@ local element = require("periodic-element.element")
 
 describe("something", function ()
     it("should work", function ()
-        print(element)
         assert.is_truthy(element)
     end)
 end)
