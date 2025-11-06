1e5567174687c106d76c5eb6286784bfdf26c2df
Snap <XxSnapperGeexX@gmail.com>
2025-11-06 00:16:06 -0800

    Init busted config file and test directory.

---
+++ test/periodic-element/busted.lua
@@ -0,0 +1,13 @@
+---@meta
+
+---@param desc string
+---@param func function()
+describe = function(desc, func) end
+
+---@param desc string
+---@param func function()
+it = function(desc, func) end
+
+assert = {
+    is_truthy = function(arg) end
+}
---
+++ test/periodic-element/element_spec.lua
@@ -0,0 +1,8 @@
+local element = require("periodic-element.element")
+
+describe("something", function ()
+    it("should work", function ()
+        print(element)
+        assert.is_truthy(element)
+    end)
+end)
