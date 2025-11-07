9f2d0a1d0130ae0dda12f03fca668cee725d1869
Snap <XxSnapperGeexX@gmail.com>
2025-11-07 15:17:59 -0800

    Delete normalize_whitespace util module.

--- src/periodic-element/util/normalize_whitespace.lua
+++
@@ -1,7 +0,0 @@
-local function normalize_whitespace(a_string)
-    a_string = a_string:match("^%s*(.-)%s*$")
-    a_string = a_string:gsub("%s+", " ")
-    return a_string
-end
-
-return normalize_whitespace
