2b34b943b0d3118cc7336c6adf79d5020aa0dfd1
Snap <XxSnapperGeexX@gmail.com>
2025-11-07 16:45:29 -0800

    Implement cli parser.

---
+++ src/periodic-element/cli.lua
@@ -0,0 +1,27 @@
+local argparse = require("argparse")
+
+local function normalize_symbol_or_name(a_string)
+    a_string = a_string:match("^%s*(.-)%s*$")
+    a_string = a_string:gsub("%s+", " ")
+    a_string = a_string:sub(1,1):upper() .. a_string:sub(2):lower()
+    return a_string
+end
+
+local parser = argparse("periodic-element", "Element of the periodic table of elements.")
+
+parser:argument("element", "An element's atomic number, symbol, or name.")
+    :args("*")
+
+parser:option("-a --atomic", "Use an element's atomic number to identify it.")
+    :args("+")
+    :convert(tonumber)
+
+parser:option("-s --symbol", "Use an element's symbol to identify it.")
+    :args("+")
+    :convert(normalize_symbol_or_name)
+
+parser:option("-n --name", "Use an element's name to identify it.")
+    :args("+")
+    :convert(normalize_symbol_or_name)
+
+return parser
---
+++ src/periodic-element/init.lua
@@ -0,0 +1,71 @@
+local parser = require("periodic-element.cli")
+local elements = require("periodic-element.elements")
+
+local args = parser:parse()
+
+local unrecognized_element_args = {}
+local element_strings = {}
+
+for i = 1, #args.element do
+    local element_arg = args.element[i]
+    local element = elements(element_arg) or elements[tonumber(element_arg)]
+
+    if element then
+        element_strings[#element_strings+1] = element:formatted_string()
+    else
+        unrecognized_element_args[#unrecognized_element_args+1] = element_arg
+    end
+end
+
+if args.atomic then
+    for i = 1, #args.atomic do
+        local number_arg = args.atomic[i]
+        local element = elements[number_arg]
+
+        if element then
+            element_strings[#element_strings+1] = element:formatted_string()
+        else
+            unrecognized_element_args[#unrecognized_element_args+1] = number_arg
+        end
+    end
+end
+
+if args.symbol then
+    for i = 1, #args.symbol do
+        local symbol_arg = args.symbol[i]
+        local element = elements(symbol_arg)
+
+        if element then
+            element_strings[#element_strings+1] = element:formatted_string()
+        else
+            unrecognized_element_args[#unrecognized_element_args+1] = symbol_arg
+        end
+    end
+end
+
+if args.name then
+    for i = 1, #args.name do
+        local name_arg = args.name[i]
+        local element = elements(name_arg)
+
+        if element then
+            element_strings[#element_strings+1] = element:formatted_string()
+        else
+            unrecognized_element_args[#unrecognized_element_args+1] = name_arg
+        end
+    end
+end
+
+if #unrecognized_element_args ~= 0 then
+    local unrecognized_elements_string = string.format(
+        'the following are unrecognized element atomic numbers, symbols, and/or names: "%s"\n\n',
+        table.concat(unrecognized_element_args, '", "')
+    )
+
+    print(unrecognized_elements_string)
+end
+
+if #element_strings ~= 0 then
+    local elements_string = table.concat(element_strings, "\n\n")
+    print(elements_string)
+end
