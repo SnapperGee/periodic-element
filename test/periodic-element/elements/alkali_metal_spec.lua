5e68af7884e2ad691a98a38e228099702623f9d5
Snap <XxSnapperGeexX@gmail.com>
2025-11-08 22:25:05 -0800

    Shorten error message.

--- test/periodic-element/elements/alkali_metal_spec.lua
+++ test/periodic-element/elements/alkali_metal_spec.lua
@@ -1,9 +1,9 @@
 local alkali_metals = require("periodic-element.elements.alkali_metal")
 
 describe("alkali_metals", function ()
-    test("length should return 6", function ()
+    test("alkali_metals:length() should return 6", function ()
         local alkali_metals_length = alkali_metals:length()
-        local message = "Expected alkali_metals:length() to return  6, but got size: " .. tostring(alkali_metals_length)
+        local message = "Expected alkali_metals:length() didn't return  6"
         assert.are.equal(6, alkali_metals_length, message)
     end)
 
