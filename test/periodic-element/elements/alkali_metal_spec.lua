3197609ff5efd2525c3af7e275d5d926cf754a90
Snap <XxSnapperGeexX@gmail.com>
2025-11-08 22:31:56 -0800

    Add tests for sodium.

--- test/periodic-element/elements/alkali_metal_spec.lua
+++ test/periodic-element/elements/alkali_metal_spec.lua
@@ -125,4 +125,123 @@ describe("alkali_metals", function ()
             assert.are.equal(0.534, lithium_density, message)
         end)
     end)
+
+    describe("sodium", function()
+        test("alkali_metals.sodium returns non nil", function ()
+            local sodium = alkali_metals.sodium
+            local message = "alkali_metals.sodium returned nil"
+            assert.is_not_nil(sodium, message)
+        end)
+
+        test("alkali_metals.sodium equals alkali_metals[11]", function ()
+            local sodium = alkali_metals.sodium
+            local sodium_via_index = alkali_metals[11]
+            local message = "alkali_metals.sodium doesn't equal alkali_metals[11]"
+            assert.are.equal(sodium, sodium_via_index, message)
+        end)
+
+        test('alkali_metals.sodium equals alkali_metals["sodium"]', function ()
+            local sodium = alkali_metals.sodium
+            local sodium_via_name = alkali_metals["sodium"]
+            local message = 'alkali_metals.sodium doesn\'t equal alkali_metals["sodium"]'
+            assert.are.equal(sodium, sodium_via_name, message)
+        end)
+
+        test('alkali_metals.sodium equals alkali_metals["na"]', function ()
+            local sodium = alkali_metals.sodium
+            local sodium_via_symbol = alkali_metals["na"]
+            local message = 'alkali_metals.sodium doesn\'t equal alkali_metals["na"]'
+            assert.are.equal(sodium, sodium_via_symbol, message)
+        end)
+
+        test('alkali_metals.sodium.name returns "Sodium"', function ()
+            local sodium_name = alkali_metals.sodium.name
+            local message = 'alkali_metals.sodium.name did not return "Sodium"'
+            assert.are.equal("Sodium", sodium_name, message)
+        end)
+
+        test('alkali_metals.sodium.symbol returns "Na"', function ()
+            local sodium_symbol = alkali_metals.sodium.symbol
+            local message = 'alkali_metals.sodium.symbol did not return "Na"'
+            assert.are.equal("Na", sodium_symbol, message)
+        end)
+
+        test("alkali_metals.sodium.number returns 11", function ()
+            local sodium_number = alkali_metals.sodium.number
+            local message = "alkali_metals.sodium.number did not return 11"
+            assert.are.equal(11, sodium_number, message)
+        end)
+
+        test("alkali_metals.sodium.mass returns 22.9897693", function ()
+            local sodium_mass = alkali_metals.sodium.mass
+            local message = "alkali_metals.sodium.mass did not return 22.9897693"
+            assert.are.equal(22.9897693, sodium_mass, message)
+        end)
+
+        test("alkali_metals.sodium.group returns 1", function ()
+            local sodium_group = alkali_metals.sodium.group
+            local message = "alkali_metals.sodium.group did not return 1"
+            assert.are.equal(1, sodium_group, message)
+        end)
+
+        test('alkali_metals.sodium.family returns "Alkali Metal"', function ()
+            local sodium_family = alkali_metals.sodium.family
+            local message = 'alkali_metals.sodium.family did not return "Alkali Metal"'
+            assert.are.equal("Alkali Metal", sodium_family, message)
+        end)
+
+        test("alkali_metals.sodium.period returns 3", function ()
+            local sodium_period = alkali_metals.sodium.period
+            local message = "alkali_metals.sodium.period did not return 3"
+            assert.are.equal(3, sodium_period, message)
+        end)
+
+        test("alkali_metals.sodium.block returns 's'", function ()
+            local sodium_block = alkali_metals.sodium.block
+            local message = "alkali_metals.sodium.block did not return 's'"
+            assert.are.equal('s', sodium_block, message)
+        end)
+
+        test("alkali_metals.sodium.electronegativity returns 0.93", function ()
+            local sodium_electronegativity = alkali_metals.sodium.electronegativity
+            local message = "alkali_metals.sodium.electronegativity did not return 0.93"
+            assert.are.equal(0.93, sodium_electronegativity, message)
+        end)
+
+        test("alkali_metals.sodium.atomic_radius returns 227", function ()
+            local sodium_atomic_radius = alkali_metals.sodium.atomic_radius
+            local message = "alkali_metals.sodium.atomic_radius did not return 227"
+            assert.are.equal(227, sodium_atomic_radius, message)
+        end)
+
+        test("alkali_metals.sodium.ionization_energy returns 5.139", function ()
+            local sodium_ionization_energy = alkali_metals.sodium.ionization_energy
+            local message = "alkali_metals.sodium.ionization_energy did not return 5.139"
+            assert.are.equal(5.139, sodium_ionization_energy, message)
+        end)
+
+        test("alkali_metals.sodium.electron_affinity returns 0.548", function ()
+            local sodium_electron_affinity = alkali_metals.sodium.electron_affinity
+            local message = "alkali_metals.sodium.electron_affinity did not return 0.548"
+            assert.are.equal(0.548, sodium_electron_affinity, message)
+        end)
+
+        test("alkali_metals.sodium.melting_point returns 370.95", function ()
+            local sodium_melting_point = alkali_metals.sodium.melting_point
+            local message = "alkali_metals.sodium.melting_point did not return 370.95"
+            assert.are.equal(370.95, sodium_melting_point, message)
+        end)
+
+        test("alkali_metals.sodium.boiling_point returns 1156", function ()
+            local sodium_boiling_point = alkali_metals.sodium.boiling_point
+            local message = "alkali_metals.sodium.boiling_point did not return 1156"
+            assert.are.equal(1156, sodium_boiling_point, message)
+        end)
+
+        test("alkali_metals.sodium.density returns 0.97", function ()
+            local sodium_density = alkali_metals.sodium.density
+            local message = "alkali_metals.sodium.density did not return 0.97"
+            assert.are.equal(0.97, sodium_density, message)
+        end)
+    end)
 end)
